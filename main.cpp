2c5106daa7fafdd26be36586655579913d7093ec
RandyWang2003 <89777788+RandyWang2003@users.noreply.github.com>
2023-03-30 00:53:55 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i =2001;
+	int a,b;
+	while(i<10000)
+	{
+		int flag =0;
+		int c =i;
+		while(c > 0)
+		{
+			a = c %16;
+			b = (c-a) /16;
+			c = b;
+			if(a<10)
+			{
+				flag =1;
+				break;
+			}
+		}
+		if(flag ==0)
+		{
+			cout<<i<<endl;
+			break;
+		}
+        i++;
+	}
+	return 0;
+}
